b23643bff837db038fc955ca287dc639f9875eb0
jang walia <jangiwalia83@gmail.com>
2023-07-19 09:03:24 +0530

    add uploadImage component

--- src/S3MediaManager.tsx
+++ src/S3MediaManager.tsx
@@ -22,6 +22,7 @@ import S3FileManager from "./S3FileManager"; // Path to your S3FileManager class
 
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import UploadImage from "./components/FileManagerActions/UploadImage";
 
 import SharedImagesButton from "./components/SharedImages";
 import PrivateImagesButton from "./components/PrivateImages";
@@ -298,43 +299,12 @@ const S3FileManagerComponent = (props: any) => {
       </div>
       {bucketConfig.canUpdate && showUpload && (
         // {bucketConfig.canCreateFolder && (
-        <div className="tw-relative tw-mb-8 tw-flex tw-w-full tw-justify-center tw-border-dashed tw-border-2">
-          <div>
-            <div
-              {...getRootProps()}
-              className="tw-w-full tw-p-10 tw-m-5 tw-flex tw-text-slate-400"
-            >
-              <input {...getInputProps()} className="" />
-              <p className="tw-w-full tw-text-center tw-font-bold tw-text-lg">
-                Drag 'n' drop some files here, or click to select files <br />
-                <em>
-                  ({MAX_FILES} files are the maximum number of files you can
-                  drop here)
-                </em>
-              </p>
-            </div>
-            <div className="tw-flex tw-w-full tw-border-t-2 tw-p-2">
-              {uploadFiles.map((file: any) => (
-                <img
-                  key={file.preview}
-                  className="tw-w-[100px] tw-object-contain tw-p-4"
-                  src={file.preview}
-                  onLoad={() => URL.revokeObjectURL(file.preview)}
-                />
-              ))}
-            </div>
-          </div>
-          <div className="tw-absolute tw-right-0">
-            <IconButton
-              onClick={() => {
-                setShowUpload(false);
-                setUploadFiles([]);
-              }}
-            >
-              <CloseIcon fontSize="large" />
-            </IconButton>
-          </div>
-        </div>
+
+        <UploadImage uploadRootProps={getRootProps} uploadInputProps={getInputProps} onClick={() => {
+          setShowUpload(false);
+          setUploadFiles([]);
+        }} MAX_FILES={MAX_FILES} uploadFiles={uploadFiles} />
+
       )}
       {bucketConfig.canUpdate && showCreateFolder && (
         <div className="tw-relative tw-w-full tw-h-40 tw-m-5 tw-flex tw-justify-center tw-border-dashed tw-border-2">
